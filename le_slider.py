7127c3b300e26b9f039589bfb1219908ab311474
Martin Berdau <martin.berdau1@uni-oldenburg.de>
2026-04-21 08:40:57 +0200

    reimplemented some vibe coded features

--- le_slider.py
+++ le_slider.py
@@ -2,11 +2,11 @@ from nicegui import ui
 import os
 import sounddevice as sd
 
-from functions.utils_gui_new import (SettingsScreen, StartDialog, PostStimulusDialog, EndScreen, RatingSlider)
+from functions.utils_gui import (SettingsScreen, StartDialog, PostStimulusDialog, EndScreen, RatingSlider)
 
 import asyncio
 import numpy as np
-
+import json
 import soundfile as sf
 
 import logging
@@ -14,12 +14,14 @@ import logging
 logger = logging.getLogger(__name__)
 
 class AudioPlayer:
-    def __init__(self, rating_slider: RatingSlider,
-                 device_id: int, blocksize: int, buffersize: int):
+    def __init__(
+            self,
+            rating_slider: RatingSlider,
+            device_id: int,
+            blocksize: int):
         self._slider = rating_slider
         self._device_id = device_id
         self._blocksize = blocksize
-        self._buffersize = buffersize
         self._idx_start = None # 0
         self.ratings = None # []
 
@@ -42,13 +44,13 @@ class AudioPlayer:
         self.ratings.append(self._slider.value)
         self._idx_start += frames
 
-    async def play_stimulus_and_record_ratings(self, audio, fs):
+    async def play_stimulus_and_record_ratings(self, filepath: str):
+        self._audio, self._fs = sf.read(filepath)
+        
         self._done = asyncio.Event()
         self._loop = asyncio.get_event_loop()
         self._idx_start = 0
         self.ratings = []
-        self._audio = audio
-        self._fs = fs
         
         with sd.OutputStream(
             callback=self._callback,
@@ -59,19 +61,6 @@ class AudioPlayer:
         ):
             await self._done.wait()
         return self.ratings
-
-
-def read_measurement_lists(base_path:str) -> list[str]:
-    txt_file_list_unsorted = [file for file in os.listdir(base_path) if '.txt' in file] # must be .txt!
-    txt_file_list = sorted(txt_file_list_unsorted)
-    return txt_file_list
-
-def read_valid_sounddevices() -> sd.DeviceList:
-    # Required: binaural playback system (TODO remove hard that constraint)
-    device_list = [d for d in sd.query_devices() if d['max_output_channels'] == 2]
-    return device_list
-
-import json
 
 def write_recordings(
         participant_id: str,
@@ -89,66 +78,74 @@ def write_recordings(
     with open(filepath, 'w', encoding='utf-8') as f:
         json.dump(recordings, f, indent=2)
 
-import matplotlib.pyplot as plt
+STIMULUS_LISTS_PATH = 'txt_lists_debugging'
+RESULTS_PATH = 'results/'
 
-async def run_experiment(): # TODO better: run_measurement()
-    results_path = 'results/'
+class MeasurementSession:
+    def __init__(self, slider:RatingSlider):
+        self._slider = slider
+        self._audio_player = None # created at runtime in self.setup()
+        self._runtime_settings = None # read at runtime in self.setup()
 
-    # INITIAL SETUP
-    slider = RatingSlider(value_init=1)
-    slider.disable()
+        self._read_measurement_lists()
+        self._read_valid_sounddevices()
 
-    # RETRIEVE RUNTIME SETTINGS FROM USER
-    txt_file_list = read_measurement_lists('txt_lists_debugging') # TODO one fixed folder for this project 
-    device_list = read_valid_sounddevices()
-    settings = await SettingsScreen(txt_file_list, device_list) # Settings for the Ratings Recorder...
+    @property
+    def measurement_lists(self) -> list[str]:
+        return self._measurement_lists
 
-    # TODO A SESSION CLASS COULD BE INSTANTIATED HERE...
+    @property
+    def valid_sounddevices(self):
+        return self._valid_sounddevices
 
-    # READ SELECTED STIMULUS LIST
-    with open(os.path.join('txt_lists_debugging', settings['stimulus_list']), 'r', encoding='utf-8') as f:
-        filepath_list = [line.strip() for line in f if line.strip()]
+    def _read_measurement_lists(self):
+        txt_file_list_unsorted = [file for file in os.listdir(STIMULUS_LISTS_PATH) if '.txt' in file] # must be .txt!
+        self._measurement_lists = sorted(txt_file_list_unsorted)
 
-    # SETTING UP AUDIO BASED ON PROVIDED SETTINGS
-    audio_player = AudioPlayer(
-        slider,
-        settings['device_id'],
-        settings['blocksize'],
-        settings['buffersize']
-    )
+    def _read_valid_sounddevices(self):
+        self._valid_sounddevices = [d for d in sd.query_devices() if d['max_output_channels'] == 2]
 
-    await StartDialog()
+    def setup(self, runtime_settings: dict):
+        """Call this just before starting the session to implement the runtime arguments provided by the user"""
+        self._runtime_settings = runtime_settings
 
-    for ii, filepath in enumerate(filepath_list):
-        data, fs = sf.read(filepath)
-        
-        slider.enable()
-        ratings = await audio_player.play_stimulus_and_record_ratings(data, fs)
-        slider.disable()
+        with open(os.path.join(STIMULUS_LISTS_PATH, self._runtime_settings['stimulus_list']), 'r', encoding='utf-8') as f:
+            self._filepath_list = [line.strip() for line in f if line.strip()]
 
-        # Saving TODO
-        write_recordings(
-            participant_id=settings['participant_id'],
-            stimulus_file=filepath,
-            output_dir=results_path,
-            recordings=ratings
-        )
+        self._audio_player = AudioPlayer(
+                self._slider,
+                runtime_settings['device_id'],
+                runtime_settings['blocksize']
+            )
 
-        if ii<len(filepath_list)-1:
-            await PostStimulusDialog() # technically could appear while saving...
-        
-    EndScreen().open()
+    async def run(self):
+        """Defines the general measurement routine"""
+        await StartDialog()
 
-    # await asyncio.to_thread(plot_ratings, audio_player.ratings, settings['blocksize'], audio_player._fs)
+        for ii, filepath in enumerate(self._filepath_list):
+            # Play & record
+            self._slider.enable()
+            ratings = await self._audio_player.play_stimulus_and_record_ratings(filepath)
+            self._slider.disable()
+            
+            # Write results
+            write_recordings(
+                participant_id=self._runtime_settings['participant_id'],
+                stimulus_file=filepath,
+                output_dir=RESULTS_PATH,
+                recordings=ratings
+            )
 
-def plot_ratings(ratings, blocksize, samplerate):
-    time_axis = np.arange(len(ratings)) * blocksize / samplerate
-    fig, ax = plt.subplots()
-    ax.plot(time_axis, ratings)
-    ax.set_xlabel('Zeit (s)')
-    ax.set_ylabel('Höranstrengung')
-    plt.show()  # blockiert bis Fenster geschlossen – daher im Thread
+            await PostStimulusDialog()
 
+        EndScreen().open()
 
-ui.timer(0, run_experiment, once=True)
+async def run_measurement():
+    slider = RatingSlider() # GUI element for continuous user rating
+    session = MeasurementSession(slider) # Manages measurement procedure
+    runtime_settings = await SettingsScreen(session.measurement_lists, session.valid_sounddevices)
+    session.setup(runtime_settings) # Setting up with args chosen at runtime
+    await session.run() # Starting measurement 
+
+ui.timer(0, run_measurement, once=True)
 ui.run(host='0.0.0.0')
